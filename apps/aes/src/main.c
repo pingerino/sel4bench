1e10086f657542f554fea3138774032a6d0feacc
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-10-27 10:17:18 +1100

    measure util once first client stops

--- apps/aes/src/main.c
+++ apps/aes/src/main.c
@@ -607,9 +607,12 @@ static void benchmark_throughput(uint64_t step, uint64_t period, tput_results_t 
             uint64_t *ipcbuffer = (uint64_t *) &(seL4_GetIPCBuffer()->msg[0]);
             seL4_BenchmarkResetLog();
 
-            benchmark_wait_children(done_ep.cptr, "B", !!a_budget + !!b_budget);
-            ZF_LOGV("Got "CCNT_FORMAT" "CCNT_FORMAT"\n", results->A[i][j], results->B[i][j]);
+            benchmark_wait_children(done_ep.cptr, "", !!b_budget);
+            /* stop measuring cpu util once one is finished */
             seL4_BenchmarkFinalizeLog();
+            benchmark_wait_children(done_ep.cptr, "", !!a_budget);
+
+            ZF_LOGV("Got "CCNT_FORMAT" "CCNT_FORMAT"\n", results->A[i][j], results->B[i][j]);
             seL4_BenchmarkGetThreadUtilisation(server_thread.tcb.cptr);
             seL4_Word idle = ipcbuffer[BENCHMARK_IDLE_LOCALCPU_UTILISATION];
             seL4_Word total = ipcbuffer[BENCHMARK_TOTAL_UTILISATION];
