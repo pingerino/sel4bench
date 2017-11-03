f98f5aa022b268364754c863e363fbf4325d7a52
Anna Lyons <Anna.Lyons@data61.csiro.au>
2017-11-03 13:52:46 +1100

    alsjda

--- libsel4benchsupport/include/ipc.h
+++ libsel4benchsupport/include/ipc.h
@@ -151,7 +151,35 @@ static const benchmark_params_t benchmark_params[] = {
         .client_prio = seL4_MaxPrio - 1,
         .server_prio = seL4_MaxPrio - 1,
         .length = 10,
-        .overhead_id = CALL_10_OVERHEAD
+        .overhead_id = CALL_10_OVERHEAD,
+        .passive = true,
+
+    },
+    /* ReplyRecv slowpath, long IPC (10), same prio server to client, on the slowpath, different address space */
+    {
+        .name        = "seL4_ReplyRecv",
+        .direction   = DIR_FROM,
+        .client_fn   = IPC_CALL_10_FUNC,
+        .server_fn   = IPC_REPLYRECV_10_FUNC,
+        .same_vspace = false,
+        .client_prio = 100,
+        .server_prio = 100,
+        .length = 10,
+        .overhead_id = REPLY_RECV_10_OVERHEAD,
+        .passive = true,
+    },
+    /* Call slowpath, long IPC (10), same prio client to server, different address space */
+    {
+        .name        = "seL4_Call",
+        .direction   = DIR_TO,
+        .client_fn   = IPC_CALL_10_FUNC2,
+        .server_fn   = IPC_REPLYRECV_10_FUNC2,
+        .same_vspace = false,
+        .client_prio = 100,
+        .server_prio = 100,
+        .length = 10,
+        .overhead_id = CALL_10_OVERHEAD,
+        .passive = false,
     },
     /* ReplyRecv slowpath, long IPC (10), same prio server to client, on the slowpath, different address space */
     {
@@ -163,7 +191,8 @@ static const benchmark_params_t benchmark_params[] = {
         .client_prio = seL4_MaxPrio - 1,
         .server_prio = seL4_MaxPrio - 1,
         .length = 10,
-        .overhead_id = REPLY_RECV_10_OVERHEAD
+        .overhead_id = REPLY_RECV_10_OVERHEAD,
+        .passive = false
     }
 };
 
